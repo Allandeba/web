60bbe13457a17e2ca5b6f43b7d05775dadd8dc49
Allan Debastiani <allandeba@yahoo.com.br>
2022-12-07 16:51:18 -0300

    Update main.js
    Using time to animate

--- Games/snake-game/Version 2/js/main.js
+++ Games/snake-game/Version 2/js/main.js
@@ -5,14 +5,18 @@ import { food } from './food.js'
 const CANVAS = document.getElementById('canvas')
 const CTX = CANVAS.getContext('2d')
 
-function animate() {
-  setTimeout(() => {
-    requestAnimationFrame(animate)
+let lastRenderTime = 0
 
-    CTX.clearRect(0, 0, CANVAS.width, CANVAS.height)
-    snake.update()
-    food.update()
-  }, 1000 / SNAKE_SPEED)
+function animate(currentTime) {
+  requestAnimationFrame(animate)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+  lastRenderTime = currentTime
+
+  CTX.clearRect(0, 0, CANVAS.width, CANVAS.height)
+  snake.update()
+  food.update()
 }
 
 animate()
