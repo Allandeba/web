874495b735c0d69e2e1d5986d9d2ea383a5e66f4
Allan Debastiani <allandeba@yahoo.com.br>
2022-12-06 22:11:56 -0300

    Update classes.js
    Only load page when all images are ready

--- Games/snake-game/Version 2/js/classes.js
+++ Games/snake-game/Version 2/js/classes.js
@@ -1,20 +1,31 @@
+const CURRENT_FRAME_START = 0
+
 export class Sprite {
-  constructor({ position, imageSrc, scale = 1, framesMax = 0, frameColumns = 1, frameRows = 1 }) {
+  constructor({ position, imageSrc, scale = 1, framesMax = 0, frameColumns = 1, frameRows = 1, numOfImages = 1 }) {
     this.position = position
     this.image = new Image()
     this.image.src = imageSrc
+    this.image.onload = this.IsImagesLoaded
     this.scale = scale
     this.framesMax = framesMax
     this.frameColumns = frameColumns
     this.frameRows = frameRows
-    this.currentFrame = 1
+    this.currentFrame = CURRENT_FRAME_START
+    this.numOfImages = numOfImages
   }
 
   draw() {}
+
   update() {
+    if (!this.IsImagesLoaded()) return
+
     if (this.currentFrame < this.framesMax) this.currentFrame++
-    else this.currentFrame = 1
+    else this.currentFrame = CURRENT_FRAME_START
 
     this.draw()
+  }
+
+  IsImagesLoaded() {
+    return !(--this.numOfImages > 0)
   }
 }
