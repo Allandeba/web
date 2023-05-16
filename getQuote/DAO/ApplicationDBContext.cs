10e68a6bcae81fee69bdba3779383b4389b75edd
Allan Debastiani <allandeba@yahoo.com.br>
2023-05-15 23:54:34 -0300

    Connect to MySQL
    Add ApplicationDBContext
    Add DotNetEnv

---
+++ getQuote/DAO/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace getQuote.DAO
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {}
+    }
+}
+
--- getQuote/Program.cs
+++ getQuote/Program.cs
@@ -1,13 +1,23 @@
-﻿namespace getQuote;
+﻿using getQuote.DAO;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
+namespace getQuote;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        DotNetEnv.Env.Load();
+
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
+
+        // Add MySQL connection.
+        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+        builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
         var app = builder.Build();
 
