be52849fc07b2f477453dafb3ab2f42375af50c9
Allan Debastiani <allandeba@yahoo.com.br>
2023-06-27 18:22:33 -0300

    fix script error

--- getQuote/wwwroot/js/site.js
+++ getQuote/wwwroot/js/site.js
@@ -2,9 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
-import * as uploadImageUtils from "./uploadImageUtils.js";
-
-const uploadFiles = document.getElementById('upload');
-uploadFiles.addEventListener('change', (event) => uploadImageUtils.processNewImages(event));
