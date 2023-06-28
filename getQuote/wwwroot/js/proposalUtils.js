04dcdefe1c7164078aed78d834a73809c5be16a4
Allan Debastiani <allandeba@yahoo.com.br>
2023-06-28 11:26:46 -0300

    feat Proposal Value column

--- getQuote/wwwroot/js/proposalUtils.js
+++ getQuote/wwwroot/js/proposalUtils.js
@@ -1,155 +1,189 @@
 ﻿const NEW_PROPOSAL_CONTENT_ID = 0;
 
 function getItemList() {
-    return document.getElementById("ItemList");
+  return document.getElementById('ItemList');
 }
 
 function setItemDefault() {
-    let itemList = getItemList();
-    itemList.value = 0;
+  let itemList = getItemList();
+  itemList.value = 0;
 }
 
 function getProposalContentTable() {
-    return document.getElementById("proposalContentTable");
+  return document.getElementById('proposalContentTable');
 }
 
 function addProposalContentCellClasses(cell) {
-    cell.classList.add("text-center");
+  cell.classList.add('text-center');
 }
 
 function setProposalContentIdContent(proposalContentIdCell) {
-    let itemList = getItemList();
-    let input = '<input type="hidden" name="ItemIdList" value="' + itemList.value + '" />';
-    let span = '<span>' + NEW_PROPOSAL_CONTENT_ID + '</span>';
+  let itemList = getItemList();
+  let input = '<input type="hidden" name="ItemIdList" value="' + itemList.value + '" />';
+  let span = '<span>' + NEW_PROPOSAL_CONTENT_ID + '</span>';
 
-    proposalContentIdCell.innerHTML = input + span;
+  proposalContentIdCell.innerHTML = input + span;
 }
 
 function setProposalContentItemIdContent(itemIdCell) {
-    let itemList = getItemList();
-    itemIdCell.textContent = itemList.value;
+  let itemList = getItemList();
+  itemIdCell.textContent = itemList.value;
 }
 
 function setProposalContentItemNameContent(itemNameCell) {
-    let itemList = getItemList();
-    itemNameCell.textContent = itemList.options[itemList.selectedIndex].text;
+  let itemList = getItemList();
+  itemNameCell.textContent = itemList.options[itemList.selectedIndex].text;
+}
+
+function setProposalContentValueContent(valueCell) {
+  let itemList = getItemList();
+  valueCell.textContent = 'Save first';
 }
 
 function setProposalContentActionContent(actionsCell) {
-    let itemList = getItemList();
-    actionsCell.innerHTML = '<span class="delete-icon" onclick="deleteItem(' + NEW_PROPOSAL_CONTENT_ID + ', ' + itemList.value + ')">' +
-        '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">' +
-        '<style>svg {fill: #e60a0a}</style>' +
-        '<path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"/>' +
-        '</svg>' +
-        '</span>';
+  let itemList = getItemList();
+  actionsCell.innerHTML =
+    '<span class="delete-icon" onclick="deleteItem(' +
+    NEW_PROPOSAL_CONTENT_ID +
+    ', ' +
+    itemList.value +
+    ')">' +
+    '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">' +
+    '<style>svg {fill: #e60a0a}</style>' +
+    '<path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"/>' +
+    '</svg>' +
+    '</span>';
 
-    //let span = document.createElement('span');
-    //span.classList.add('delete-icon');
-    //span.addEventListener('onclick', () => {
-    //    deleteItem(NEW_PROPOSAL_CONTENT_ID, itemList.value);
-    //});
+  //let span = document.createElement('span');
+  //span.classList.add('delete-icon');
+  //span.addEventListener('onclick', () => {
+  //    deleteItem(NEW_PROPOSAL_CONTENT_ID, itemList.value);
+  //});
 
-    //let svg = document.createElement('svg');
-    //svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    //svg.setAttribute('height', '1em');
-    //svg.setAttribute('viewBox', '0 0 448 512');
+  //let svg = document.createElement('svg');
+  //svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  //svg.setAttribute('height', '1em');
+  //svg.setAttribute('viewBox', '0 0 448 512');
 
-    //let style = document.createElement('style');
-    //style.innerHTML = 'svg {fill: #e60a0a}';
-    //svg.appendChild(style);
+  //let style = document.createElement('style');
+  //style.innerHTML = 'svg {fill: #e60a0a}';
+  //svg.appendChild(style);
 
-    //let path = document.createElement('path');
-    //path.setAttribute('d', 'M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z');
-    //svg.appendChild(path);
+  //let path = document.createElement('path');
+  //path.setAttribute('d', 'M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z');
+  //svg.appendChild(path);
 
-    //span.appendChild(svg);
-    //actionsCell.appendChild(span);
+  //span.appendChild(svg);
+  //actionsCell.appendChild(span);
 }
 
 function createProposalContentCells(proposalContentRow) {
+  let proposalContentIdCell = proposalContentRow.insertCell();
+  let itemIdCell = proposalContentRow.insertCell();
+  let itemNameCell = proposalContentRow.insertCell();
+  let valueCell = proposalContentRow.insertCell();
+  let actionsCell = proposalContentRow.insertCell();
 
-    let proposalContentIdCell = proposalContentRow.insertCell();
-    let itemIdCell = proposalContentRow.insertCell();
-    let itemNameCell = proposalContentRow.insertCell();
-    let actionsCell = proposalContentRow.insertCell();
+  addProposalContentCellClasses(proposalContentIdCell);
+  addProposalContentCellClasses(itemIdCell);
+  addProposalContentCellClasses(itemNameCell);
+  addProposalContentCellClasses(valueCell);
+  addProposalContentCellClasses(actionsCell);
 
-    addProposalContentCellClasses(proposalContentIdCell);
-    addProposalContentCellClasses(itemIdCell);
-    addProposalContentCellClasses(itemNameCell);
-    addProposalContentCellClasses(actionsCell);
+  setProposalContentIdContent(proposalContentIdCell);
+  setProposalContentItemIdContent(itemIdCell);
+  setProposalContentItemNameContent(itemNameCell);
+  setProposalContentValueContent(valueCell);
+  setProposalContentActionContent(actionsCell);
+}
 
-    setProposalContentIdContent(proposalContentIdCell);
-    setProposalContentItemIdContent(itemIdCell);
-    setProposalContentItemNameContent(itemNameCell);
-    setProposalContentActionContent(actionsCell);
+function getNewRowPosition() {
+  const NEW_ROW_POSITION = proposalContentTable.rows.length - 1;
+  return NEW_ROW_POSITION == 0 ? 1 : NEW_ROW_POSITION;
 }
 
 function createNewProposalContentTableRow() {
-    let rowId = 'proposalContentRow_' + NEW_PROPOSAL_CONTENT_ID;
-    let row = proposalContentTable.insertRow();
-    row.id = rowId;
+  let rowId = 'proposalContentRow_' + NEW_PROPOSAL_CONTENT_ID;
+  let row = proposalContentTable.insertRow(getNewRowPosition());
+  row.id = rowId;
 
-    createProposalContentCells(row);
+  createProposalContentCells(row);
 
-    return row;
+  return row;
 }
 
 function selectionChange() {
-    let itemList = getItemList();
-    if (itemList.value == 0) return;
+  let itemList = getItemList();
+  if (itemList.value == 0) return;
 
-    let proposalContentTable = getProposalContentTable();
+  let proposalContentTable = getProposalContentTable();
 
-    if (isItemInTable(itemList.value)) {
-        alert('O item já existe na tabela!');
-        setItemDefault();
-        return;
-    }
-
-    createNewProposalContentTableRow();
+  if (isItemInTable(itemList.value)) {
+    alert('O item já existe na tabela!');
     setItemDefault();
+    return;
+  }
+
+  createNewProposalContentTableRow();
+  setItemDefault();
 }
 
 function isItemInTable(itemId) {
-    let proposalContentTable = getProposalContentTable();
+  let proposalContentTable = getProposalContentTable();
 
-    for (let i = 0; i < proposalContentTable.rows.length; i++) {
-        let row = proposalContentTable.rows[i];
-        let rowItemId = row.cells[1].textContent; // A célula 1 contém o ID do item
+  for (let i = 0; i < proposalContentTable.rows.length; i++) {
+    let row = proposalContentTable.rows[i];
+    let rowItemId = row.cells[1].textContent; // A célula 1 contém o ID do item
 
-        // Verifica se o ID do item já existe na tabela
-        if (rowItemId == itemId) {
-            return true;
-        }
+    // Verifica se o ID do item já existe na tabela
+    if (rowItemId == itemId) {
+      return true;
     }
+  }
 
-    return false;
+  return false;
 }
 
 function deleteItemOnServer(proposalContentId) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/Proposal/DeleteProposalContent/' + proposalContentId);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            alert('Item removido com sucesso!')
-        } else {
-            alert('Ocorreu um erro ao deletar o item!')
-        }
-    };
-    xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/Proposal/DeleteProposalContent/' + proposalContentId);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      alert('Item removido com sucesso!');
+    } else {
+      alert('Ocorreu um erro ao deletar o item!');
+    }
+  };
+  xhr.send();
+}
+
+function calculateTotalValue() {
+  let proposalContentTable = getProposalContentTable();
+  let totalValueCell = document.getElementById('total-value-cell');
+  let totalValue = 0;
+
+  for (let i = 0; i < proposalContentTable.rows.length; i++) {
+    let row = proposalContentTable.rows[i];
+    let itemValueCell = row.querySelector('#item-value');
+    if (itemValueCell) {
+      let itemValue = parseFloat(itemValueCell.textContent.replace('R$ ', ''));
+      totalValue += itemValue;
+    }
+  }
+
+  totalValueCell.textContent = 'R$ ' + totalValue.toFixed(2);
 }
 
 function deleteItem(proposalContentId, itemId) {
-    if (confirm('Tem certeza de que deseja excluir o Proposal Content ID: ' + proposalContentId + ', Item ID: ' + itemId + '?')) {
-        var row = document.getElementById('proposalContentRow_' + proposalContentId);
-        if (row) {
-            if (proposalContentId && proposalContentId != 0) {
-                deleteItemOnServer(proposalContentId);
-            }
-
-            row.remove();
-        }
+  if (confirm('Tem certeza de que deseja excluir o Proposal Content ID: ' + proposalContentId + ', Item ID: ' + itemId + '?')) {
+    var row = document.getElementById('proposalContentRow_' + proposalContentId);
+    if (row) {
+      if (proposalContentId && proposalContentId != 0) {
+        deleteItemOnServer(proposalContentId);
+      }
+      row.remove();
+      calculateTotalValue();
     }
+  }
 }
