cc6272f4b42ac00d767221578bad9c9c181838df
Allan Debastiani <allandeba@yahoo.com.br>
2023-06-28 13:10:03 -0300

    remove unused code

--- getQuote/wwwroot/js/proposalUtils.js
+++ getQuote/wwwroot/js/proposalUtils.js
@@ -53,28 +53,6 @@ function setProposalContentActionContent(actionsCell) {
     '<path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"/>' +
     '</svg>' +
     '</span>';
-
-  //let span = document.createElement('span');
-  //span.classList.add('delete-icon');
-  //span.addEventListener('onclick', () => {
-  //    deleteItem(NEW_PROPOSAL_CONTENT_ID, itemList.value);
-  //});
-
-  //let svg = document.createElement('svg');
-  //svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-  //svg.setAttribute('height', '1em');
-  //svg.setAttribute('viewBox', '0 0 448 512');
-
-  //let style = document.createElement('style');
-  //style.innerHTML = 'svg {fill: #e60a0a}';
-  //svg.appendChild(style);
-
-  //let path = document.createElement('path');
-  //path.setAttribute('d', 'M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z');
-  //svg.appendChild(path);
-
-  //span.appendChild(svg);
-  //actionsCell.appendChild(span);
 }
 
 function createProposalContentCells(proposalContentRow) {
@@ -115,8 +93,6 @@ function createNewProposalContentTableRow() {
 function selectionChange() {
   let itemList = getItemList();
   if (itemList.value == 0) return;
-
-  let proposalContentTable = getProposalContentTable();
 
   if (isItemInTable(itemList.value)) {
     alert('O item já existe na tabela!');
