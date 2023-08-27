0e48b6e7084583a5479f9f5a381109e1f63a06b9
Allan Debastiani <allandeba@yahoo.com.br>
2023-08-27 12:35:41 -0300

    fix Contact.modelValidation

--- getQuote/Models/ContactModel.cs
+++ getQuote/Models/ContactModel.cs
@@ -8,14 +8,14 @@ public class ContactModel
     [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
     public int ContactId { get; set; }
 
-    [Required]
-    [MaxLength(50)]
-    [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email format.")]
+    [Required(ErrorMessage = Messages.EmptyTextValidation)]
+    [MaxLength(50, ErrorMessage = Messages.MaxLengthValidation)]
+    [DataType(DataType.EmailAddress, ErrorMessage = Messages.InvalidFormatValidation)]
     public string Email { get; set; } = String.Empty;
 
-    [Required]
-    [MaxLength(15)]
-    [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Phone format.")]
+    [Required(ErrorMessage = Messages.EmptyTextValidation)]
+    [MaxLength(15, ErrorMessage = Messages.MaxLengthValidation)]
+    [DataType(DataType.PhoneNumber, ErrorMessage = Messages.InvalidFormatValidation)]
     public string Phone { get; set; } = String.Empty;
 
     public int PersonId { get; set; }
