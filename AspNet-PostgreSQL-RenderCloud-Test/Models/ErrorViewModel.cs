e96875515dcbb82b7bff97af4ec9fd61ec6c89f6
Allan Debastiani <allandeba@yahoo.com.br>
2023-09-13 23:16:06 -0300

    renderCloud docker test

--- AspNet-PostgreSQL-RenderCloud-Test/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AspNet_PostgreSQL_RenderCloud_Test.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
